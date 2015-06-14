c7ce8f3f5774ab9d6bad3f8b158f08f9d911d49d
Nicolai Czempin <nicolai.czempin@gmail.com>
2015-06-14 13:07:42 +0200

    cleanup formatting, separate logic from constructor

--- Project Roguelike/workspace/Roguelike/src/mainPack/StartButtonFunktion.java
+++ Project Roguelike/workspace/Roguelike/src/mainPack/StartButtonFunktion.java
@@ -3,18 +3,14 @@ package mainPack;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+public class StartButtonFunktion implements ActionListener {
 
-public class StartButtonFunktion implements ActionListener{
+	@Override
+	public void actionPerformed(final ActionEvent arg0) {
+		// Startet Spiel
+		final Client client = new Client();
+		client.run();
 
-	
-	public void actionPerformed(ActionEvent arg0) {
-		//Startet Spiel
-		@SuppressWarnings("unused")
-		Client client = new Client();
-		
-		
 	}
-	
-	
 
 }
